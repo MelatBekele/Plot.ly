19a12c5daaceba7553908f3fbc281487cc468ec3
Melat Bekele <27melat28@gmail.com>
2021-05-16 02:00:01 -0400

    updating top 10

--- app.js
+++ app.js
@@ -11,28 +11,39 @@ function getplots(id) {
     
     console.log(data);
    //selecting the values
-    var sampleValues = data.samples[0].sample_values;
-    var otu_ids = data.samples[0].otu_ids;
+    var sampleValues = data.samples[0].sample_values.slice(0,10);
+    console.log(sampleValues);
+    var otuIds = data.samples[0].otu_ids;
     var otuLables = data.samples[0].otu_labels;
     //var sampleValuess = unpack(data.samples.sample_values,3);
       
     //Top 10 
-    var topTen = data.samples[0].sample_values.slice(0,10).sort((a,b) =>b-a);
+    var topTen = data.samples[0].sample_values.slice(0,10).sort((a,b) =>a-b);
+    var otuTenIds = data.samples[0].otu_ids.slice(0,10);
     var topTenLa = data.samples[0].otu_labels.slice(0,10);
 
-    //formating havor over and label 
+    //formating ticker and label 
+    var topTenMap = otuTenIds.map(d => "OTU " + d);
+    console.log(topTenMap);
+
     
-
-
-    console.log(topTen);
-    var data = [{
+    var Hgraph = {
         type: 'bar',
-        x: sampleValues,
-        y: otu_ids,
+        x: topTen,
+        y: topTenMap,
+        text: topTenLa,
         orientation: 'h'
-      }];
-      
-      Plotly.newPlot('bar', data);
+      };
+    
+      var layout = {
+        title: "Top 10 Samples",
+        xaxis: { title: "Samples" },
+        yaxis: { title: "ID",
+        tickmode:"linear",}
+
+      };
+      var data = [Hgraph];
+      Plotly.newPlot('bar', data, layout);
   });
 
 
