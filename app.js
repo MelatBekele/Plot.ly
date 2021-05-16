2ee6535ca636f7a5a1b6ced9b3215044f334d79b
Melat Bekele <27melat28@gmail.com>
2021-05-16 01:03:15 -0400

    slicing to to 10

--- app.js
+++ app.js
@@ -8,14 +8,23 @@
 function getplots(id) {
 
   d3.json('samples.json').then(function(data) {
+    
     console.log(data);
-
+   //selecting the values
     var sampleValues = data.samples[0].sample_values;
     var otu_ids = data.samples[0].otu_ids;
     var otuLables = data.samples[0].otu_labels;
     //var sampleValuess = unpack(data.samples.sample_values,3);
-      console.log(otuLables);
-     
+      
+    //Top 10 
+    var topTen = data.samples[0].sample_values.slice(0,10).sort((a,b) =>b-a);
+    var topTenLa = data.samples[0].otu_labels.slice(0,10);
+
+    //formating havor over and label 
+    
+
+
+    console.log(topTen);
     var data = [{
         type: 'bar',
         x: sampleValues,
