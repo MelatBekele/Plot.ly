dc219026eadcb6248dc84f79e524bc48ac8a85ef
Melat Bekele <27melat28@gmail.com>
2021-05-18 00:18:26 -0400

    addd lines

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ function getplots(id) {
 
   d3.json('samples.json').then(function(data) {
     
-    console.log(data);
+   // console.log(data);
    //selecting the values
     var sampleValues = data.samples[0].sample_values;
     var sampleValuesVer = data.samples[0].sample_values.slice(0,10);
-    console.log(sampleValuesVer);
+    //console.log(sampleValuesVer);
     var otuIdss = data.samples[0].otu_ids;
-    console.log(otuIdss);
+    //console.log(otuIdss);
     var otuLables = data.samples[0].otu_labels;
     //var sampleValuess = unpack(data.samples.sample_values,3);
       
@@ -26,7 +26,7 @@ function getplots(id) {
 
     //formating ticker and label 
     var topTenMap = otuTenIds.map(d => "OTU " + d);
-    console.log(topTenMap);
+    //console.log(topTenMap);
 
     
     var Hgraph = {
@@ -77,3 +77,33 @@ function getplots(id) {
 }
 
 getplots();
+
+
+//Display the sample metadata, i.e., an individual's demographic information.
+function smpMetadata (id) {
+  
+  d3.json('samples.json').then(function(medata) {
+    console.log(medata);
+    
+    //var metaValues = medata.metadata;
+    //console.log(metaValues);
+
+    var filtermeta = medata.metadata.filter(datacpy => datacpy.id.toString() === id)[0];
+    console.log(filtermeta);
+
+    var demographics = d3.select("#sample-metadata");
+    console.log(demographics);
+
+    d3.select("#sample-metadata").node().value = "";
+ 
+
+    Object.entries(filtermeta).forEach((key) => {   
+      demographics.append("selDataset");    
+     });
+  });
+}
+
+//function connection(){
+//getplots();
+smpMetadata("941");
+//}
